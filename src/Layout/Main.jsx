d7a3ae7ef44b2d9f5d4e7fc0cb253c45dd59b829
H.M Sadiq <hmsadikurrahman120@gmail.com>
2025-03-17 11:39:39 +0600

    start edu main landing demo

---
+++ src/Layout/Main.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Home from "../Pages/Home";
+import Navber from "../Pages/Navber/Navber";
+import Footer from "../Pages/Footer/Footer";
+import { Outlet, useLocation } from "react-router-dom";
+
+const Main = () => {
+  const { pathname } = useLocation();
+  console.log(pathname);
+
+  return (
+    <div>
+      <div className={`${pathname === "/login" ? "hidden" : "block"}`}>
+        <Navber />
+      </div>
+      <Outlet />
+      <div className={`${pathname === "/login" ? "hidden" : "block"}`}>
+        <Footer />
+      </div>
+    </div>
+  );
+};
+
+export default Main;
