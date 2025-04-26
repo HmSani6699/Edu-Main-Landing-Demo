01ec6f716a3a1b42ceb14c993ea25762cda0cbb6
H.M Sadiq <hmsadikurrahman120@gmail.com>
2025-04-26 13:27:41 +0600

    added homework review list

--- src/Pages/Dashboard/Teacher/TeacherMobileDashboard/TeacherHomwWork/HomeworkReviewGrad/HomeworkReviewGrad.jsx
+++ src/Pages/Dashboard/Teacher/TeacherMobileDashboard/TeacherHomwWork/HomeworkReviewGrad/HomeworkReviewGrad.jsx
@@ -1,7 +1,31 @@
 import React from "react";
+import HomeworkReviewGradCard from "./HomeworkReviewGradCard";
 
 const HomeworkReviewGrad = () => {
-  return <div>review</div>;
+  return (
+    <div className="px-[10px] flex flex-col gap-[20px] my-[30px] ">
+      <HomeworkReviewGradCard
+        subject={"Bangla"}
+        assigenDate={"10 April 2025"}
+        submissionDate={"10 April 2025"}
+      />
+      <HomeworkReviewGradCard
+        subject={"Bangla"}
+        assigenDate={"10 April 2025"}
+        submissionDate={"10 April 2025"}
+      />
+      <HomeworkReviewGradCard
+        subject={"Bangla"}
+        assigenDate={"10 April 2025"}
+        submissionDate={"10 April 2025"}
+      />
+      <HomeworkReviewGradCard
+        subject={"Bangla"}
+        assigenDate={"10 April 2025"}
+        submissionDate={"10 April 2025"}
+      />
+    </div>
+  );
 };
 
 export default HomeworkReviewGrad;
---
+++ src/Pages/Dashboard/Teacher/TeacherMobileDashboard/TeacherHomwWork/HomeworkReviewGrad/HomeworkReviewGradCard.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import Reveal from "../../../../../../Component/Reveal/Reveal";
+import subject_image from "../../../../../../../public/test.png";
+
+const HomeworkReviewGradCard = ({ subject, assigenDate, submissionDate }) => {
+  const navigate = useNavigate();
+  return (
+    <Reveal>
+      <div
+        onClick={() => navigate("/student-class-performance")}
+        className="border-[#07A698] border rounded-[20px] p-[20px] flex items-center justify-between"
+      >
+        <div>
+          <button className="px-[10px] bg-[#07a69824] text-[#07a698] rounded-[5px] mb-[10px]">
+            <h2 className="text-[23px] ">{subject}</h2>
+          </button>
+          <p className="text-gray-400">{assigenDate}</p>
+          <p className="text-gray-400">{submissionDate}</p>
+        </div>
+        <div className=" h-[80px] w-[70px] rounded-[5px]">
+          <img
+            className="w-full h-full rounded-[5px]"
+            src={subject_image}
+            alt="subjct"
+          />
+        </div>
+      </div>
+    </Reveal>
+  );
+};
+
+export default HomeworkReviewGradCard;
